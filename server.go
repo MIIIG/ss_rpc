a39974bbb9fb912d2565c5dc5a0ef23714b6091f
MIIIG <arthur.badretdinov@gmail.com>
2022-07-08 13:17:34 +0500

    error handling, server log

--- server.go
+++ server.go
@@ -3,10 +3,9 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io"
 	"io/ioutil"
+	"log"
 	"net/http"
-	"os"
 )
 
 type Result struct {
@@ -15,64 +14,89 @@ type Result struct {
 	Result  string `json:"result"`
 }
 type Request struct {
-	Id      string `json:"id"`
-	Jsonrpc string `json:"jsonrpc"`
-	Method  string `json:"method"`
-	Params  Params `json:"params"`
+	Id      string         `json:"id"`
+	Jsonrpc string         `json:"jsonrpc"`
+	Method  string         `json:"method"`
+	Params  map[string]any `json:"params"`
 }
 
-type Params struct {
-	Name string `json:"name"`
+type Error struct {
+	Id      string    `json:"id"`
+	Jsonrpc string    `json:"jsonrpc"`
+	Error   RpcErrors `json:"error"`
+}
+
+type RpcErrors struct {
+	Code    string `json:"code"`
+	Message string `json:"message"`
+}
+
+var myErr = map[string]RpcErrors{
+	"InternalError":  {"-32603", "Internal error"},
+	"ParseError":     {"-32700", "Parse error"},
+	"MethodNotFound": {"-32601", "Method not found"},
 }
 
 func Handler(w http.ResponseWriter, r *http.Request) {
 	var req Request
 
-	switch r.Method {
-	case "GET":
-		w.WriteHeader(200)
-		io.WriteString(w, "This is RPC server!\n")
-	case "POST":
-		reqbody, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			fmt.Printf("server: could not read request body: %s\n", err)
+	reqbody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		ErrorHandler("InternalError", w)
+	}
+	jbody := json.Unmarshal(reqbody, &req)
+	if jbody != nil {
+		ErrorHandler("ParseError", w)
+	}
+	if req.Method != "" {
+		switch req.Method {
+		case "greeting":
+			Greeting(req, w)
+		default:
+			NotFound(req, w)
 		}
-		jbody := json.Unmarshal(reqbody, &req)
-		if jbody != nil {
-			fmt.Printf("Can't unmarshall data: %s\n", jbody)
-		}
-		MethodHandler(req.Method, req, w)
-	default:
-		w.WriteHeader(http.StatusMethodNotAllowed)
 	}
 }
 
-func MethodHandler(method string, request Request, w http.ResponseWriter) {
-	switch method {
-	case "greeting":
-		Greeting(request, w)
-	default:
-		io.WriteString(w, "Nothing to do!\n")
-	}
+func ErrorHandler(errtype string, w http.ResponseWriter) {
+	var output = Error{"null", "2.0", myErr[errtype]}
+	js, _ := json.Marshal(output)
+	WriteJSON(js, w)
 }
 
 func Greeting(request Request, w http.ResponseWriter) {
 	var output Result
-	result := fmt.Sprintf("Hello, %s", request.Params.Name)
+	result := fmt.Sprintf("Hello, %s", request.Params["name"])
 	output = Result{request.Id, request.Jsonrpc, result}
 	js, err := json.Marshal(output)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		ErrorHandler("InternalError", w)
 	}
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(js)
+	WriteJSON(js, w)
+}
+
+func NotFound(request Request, w http.ResponseWriter) {
+	var output = Error{request.Id, request.Jsonrpc, myErr["MethodNotFound"]}
+	js, err := json.Marshal(output)
+	if err != nil {
+		ErrorHandler("InternalError", w)
+	}
+	WriteJSON(js, w)
+}
+
+func WriteJSON(data []byte, w http.ResponseWriter) {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.Write(data)
 }
 
 func main() {
-	http.HandleFunc("/", Handler)
-	err := http.ListenAndServe(":8000", nil)
-	if err != nil {
-		fmt.Printf("error starting server: %s\n", err)
-		os.Exit(1)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", Handler)
+	srv := &http.Server{
+		Handler: mux,
+		Addr:    "127.0.0.1:8000",
 	}
+
+	log.Println("Air on", srv.Addr)
+	log.Fatal(srv.ListenAndServe())
 }
